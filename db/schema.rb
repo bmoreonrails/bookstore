87fedb945d746885018bb0351e3909f2ae7cf04c
Ali Ibrahim <aibrahim2k2@gmail.com>
2016-12-26 21:39:40 -0500

    Continue Chapter 4 by adding a description column to the books table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115030350) do
+ActiveRecord::Schema.define(version: 20161227020938) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20161115030350) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "quantity"
+    t.text     "description"
   end
 
 end

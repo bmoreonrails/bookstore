c6c78192d13a9157cb975dcd67c2f0020900a511
Ali Ibrahim <aibrahim2k2@gmail.com>
2016-12-27 20:37:58 -0500

    Continue Chapter 5 by creating new books in BookController#create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,4 +11,8 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
+
+  def create
+    Book.create(title: params[:book][:title], author: params[:book][:author], price_cents: params[:book][:price_cents], quantity: params[:book][:quantity], description: params[:book][:description])
+  end
 end

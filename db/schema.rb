f342efdef16cbe782743db2cfabeeb8ef8e0d28f
Ali Ibrahim <aibrahim2k2@gmail.com>
2017-01-03 17:39:08 -0500

    Start Chapter 9
      * Generate the review model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161227020938) do
+ActiveRecord::Schema.define(version: 20170103220053) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20161227020938) do
     t.datetime "updated_at",  null: false
     t.integer  "quantity"
     t.text     "description"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

5f2d26981cb22e0592fcda0836d1227519301c3c
Ali Ibrahim <aibrahim2k2@gmail.com>
2017-01-07 19:16:53 -0500

    Continue Chapter 11 by redirecting to the books_path after creating a review for the requested book

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,6 +7,7 @@ class ReviewsController < ApplicationController
   def create
     book = Book.find(params[:book_id])
     book.reviews.create(review_params)
+    redirect_to book_path(book)
   end
 
   def review_params

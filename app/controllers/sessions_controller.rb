ad412c545f94d8ea7cb46eef62fad43bc84a9db9
Ali Ibrahim <aibrahim2k2@gmail.com>
2017-01-14 16:11:12 -0500

    Continue Chapter 13
      * Flush out SessionsController#create
      * Update the application's layout template so flash messages are rendered

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+
+    if user.authenticate(params[:session][:password])
+      flash[:notice] = "Welcome back, #{user.username}!"
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash[:alert] = "Sorry, your username or password is invalid."
+      render :new
+    end
+  end
 end

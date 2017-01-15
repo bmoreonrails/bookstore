b7333c686e77035da83fdcb030fa4649a8416552
Ali Ibrahim <aibrahim2k2@gmail.com>
2017-01-14 23:03:21 -0500

    Finish Chapter 13 by adding a log out link

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,4 +14,10 @@ class SessionsController < ApplicationController
       render :new
     end
   end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "See ya next time!"
+    redirect_to root_path
+  end
 end

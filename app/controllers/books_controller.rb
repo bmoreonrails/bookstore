03bfff567763aeac13dcea655939835c3d4cb2cc
Ali Ibrahim <aibrahim2k2@gmail.com>
2017-01-19 22:22:54 -0500

    Continue Chapter 14 by securing the BooksController

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :verify_user_session
+
   def index
     @books = Book.all
   end
@@ -35,5 +37,12 @@ class BooksController < ApplicationController
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
+  end
+
+  def verify_user_session
+    if session[:user_id].blank?
+      flash[:alert] = "Please login to continue"
+      redirect_to new_session_path
+    end
   end
 end
